9d0ced8fce574b3b1e16b6612d66f4bada5b215b
rish-16 <mail.rishabh.anand@gmail.com>
2021-01-18 19:54:20 +0800

    Add Bieber songs; Update download script

--- src/audio_dl.py
+++ src/audio_dl.py
@@ -1,7 +1,7 @@
 import os, youtube_dl
 
 ydl_opts = {
-    'outtmpl': './bin/%(title)s.%(ext)s',
+    'outtmpl': './bin/adele/%(title)s.%(ext)s',
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -21,11 +21,39 @@ adele = [
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(adele)
     
-path = "./bin/"    
+adele_path = "./bin/adele/"
 ctr = 0
 
-for file in os.listdir(path):
-    filepath = path + file
-    print (filepath)
-    os.rename(filepath, path + "audio_{}.mp3".format(str(ctr)))
+for file in os.listdir(adele_path):
+    filepath = adele_path + file
+    os.rename(filepath, adele_path + "audio_adele_{}.mp3".format(str(ctr)))
+    ctr += 1    
+
+ydl_opts = {
+    'outtmpl': './bin/bieber/%(title)s.%(ext)s',
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3'
+    }]
+}    
+
+bieber = [
+    "https://www.youtube.com/watch?v=8ELbX5CMomE", # sorry
+    "https://www.youtube.com/watch?v=h2jvHynuMjI", # stuck with u
+    "https://www.youtube.com/watch?v=EaMed9sUPVo", # yummy
+    "https://www.youtube.com/watch?v=ztHWxFVk9jE", # intentions
+    "https://www.youtube.com/watch?v=Uim4GwSfzxY", # baby
+    "https://www.youtube.com/watch?v=nPfg2Qymj8U", # love yourself
+]
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(bieber)
+    
+bieber_path = "./bin/bieber/"
+ctr = 0
+
+for file in os.listdir(bieber_path):
+    filepath = bieber_path + file
+    os.rename(filepath, bieber_path + "audio_bieber_{}.mp3".format(str(ctr)))
     ctr += 1
